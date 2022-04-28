09602fcf312dbf5e1ade234dc0634b204e3ce456
Nikita <mykyta.kalyna@apriorit.com>
2022-04-28 11:12:04 +0300

    TASK

---
+++ index.js
@@ -0,0 +1,11 @@
+var URL ="https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5"
+$('#clk').on('click',function(){
+    console.log('aaaaa')
+    $.ajax({
+        url:URL
+
+    }).done(function(res){
+        console.log(res)
+        $('#res').text(res)
+    })
+})
